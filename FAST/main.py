d48c99805a2240cb9798e86853c9cff7833cfb6b
Nyxxs12222 <122043017@upq.edu.mx>
2025-01-30 16:50:13 -0600

    AvancePractica2.2
    El codigo que termine obteniendo

--- FAST/main.py
+++ FAST/main.py
@@ -1,24 +1,43 @@
 from fastapi import FastAPI
+from typing import Optional
 
-app= FastAPI(
-    title = 'Mi primerAPI 192',
-    description = 'Rberto Uriel Martinez Martinez',
-    version = '1.0.1'
+app = FastAPI(
+    title='Mi primer API 192',
+    description='Roberto Uriel Martinez Martinez',
+    version='1.0.1'
 )
 
-#Endpoint home 
-@app.get('/', tags = ['Hola Mundo'])
-def home():
-    return {'hello':'wordl FastAPI'}
+usuarios = [
+    {"id": 1, "nombre": "Uriel", "edad": 20},
+    {"id": 2, "nombre": "Isay", "edad": 37},
+    {"id": 3, "nombre": "Evel", "edad": 20},
+    {"id": 4, "nombre": "Ana", "edad": 18}
+]
 
-#Endpoint promedio 
-@app.get('/promedio', tags = ['Promedios'])
+# Endpoint home
+@app.get('/', tags=['Hola Mundo'])
+def home():
+    return {'hello': 'world FastAPI'}
+
+# Endpoint promedio
+@app.get('/promedio', tags=['Promedios'])
 def promedio():
     return 5.5
 
-#Endpoint parametro Obligactorio 
-@app.get('/usuario/{id}', tags = ['Parametro obligatorio'])
-def consultaUsuario(id:int):
-    #conectamos a la BD
-    #consultamos
-    return {'Se encontro el usuario':id}
+# Endpoint parámetro obligatorio
+@app.get('/usuario/{id}', tags=['Parámetro obligatorio'])
+def consultaUsuario(id: int):
+    # conectamos a la BD
+    # consultamos
+    return {'Se encontró el usuario': id}
+
+# Endpoint parámetro opcional
+@app.get('/usuario/', tags=['Parámetro Opcional'])
+def consultaUsuario2(id: Optional[int] = None):
+    if id is not None:
+        for usu in usuarios:
+            if usu["id"] == id:
+                return {"mensaje": "Usuario encontrado", "usuario": usu}
+        return {"mensaje": f"No se encontró el usuario con id: {id}"}
+    else:
+        return {"mensaje": "No se proporcionó un id"}
